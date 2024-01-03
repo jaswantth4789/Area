d9215173084e996743cafcd7eefcaa9209907c4c
jaswantth potluri <potlurivenkatajaswantth@gmail.com>
2024-01-03 09:50:16 +0000

    Refactor. added better assertions

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.rectangle;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,7 @@ public class RectangleTest{
     void shouldReturnAreaOfRectangleAs50WhenLengthIs10AndBreadthIs5(){
         Area area= new Area();
         int result=area.RectangleArea(10,5);
-        assertEquals(50,result);
+        assertThat(result,is(equalTo(50)));
 
     }
 }
